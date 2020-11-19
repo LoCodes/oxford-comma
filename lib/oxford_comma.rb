f0e48943f02b5628757f258820d0821fd8ed3fa7
Loren Cipriano <loren25cipriano@gmail.com>
2020-11-19 11:57:13 -0800

    commit tree 528fb277ba0343d3c176d29bd0690286358717d4

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,23 @@
-def oxford_comma(array)
+require 'pry'
 
-end
+def oxford_comma(array)
+    if array.size == 2
+      return "#{array[0]} and #{array[1]}"
+    elsif 2 <= array.size
+      array[-1].insert(0, "and ")
+    end
+    array.join(", ") 
+  end
+
+
+
+
+# #oxford_comma
+#   returns a string without any additional formatting when given a 1-element array
+#   adds 'and' between elements when given a 2-element array
+#   adds commas plus a final 'and' when given a 3-element array
+#   correctly formats arrays of lengths greater than three
+
+
+
+
